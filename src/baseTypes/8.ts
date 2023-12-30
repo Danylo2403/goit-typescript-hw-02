07575ad5cb523a5cf57714754fbd337190fa4326
Danylo2403 <daniilkon24@gmail.com>
2023-12-30 02:09:35 +0200

    baseTypes + generic

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -2,7 +2,9 @@
   Створіть тип "Gender", використовуючи union type, 
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
-
-const myGender;
+// створення типу "Gender" з union type
+type Gender = "male" | "female";
+// створення myGender з типом Gender
+const myGender: Gender = "male";
 
 export {};
--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,24 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface ComponentProps {
+  id: number;
+  name: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+// Клас компонента, що використовує generics
+class Component<T extends ComponentProps> {
+  constructor(public props: T) {}
+}
+
+// Інтерфейс для пропсів сторінки, що розширює базовий інтерфейс компонента
+interface PageProps extends ComponentProps {
+  title: string;
+}
+
+// Клас сторінки, що успадковує від компонента та використовує generics
+class Page extends Component<PageProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
